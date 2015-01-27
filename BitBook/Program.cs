3abe7055006a4aff4789778fa0d2a87398c448e0
Rhys Laval <rhyslaval@gmail.com>
2015-01-27 17:13:18 +0000

    FleckLog

--- BitBook/Program.cs
+++ BitBook/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
+using Fleck;
 
 namespace BitBook
 {
 	class MainClass
 	{
+
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Starting up BitBook!");
+			FleckLog.Info ("Starting up BitBook!");
 			// Start up static web server
 			using (var host = new StaticSelfHost.StaticSelfHost ("56001"))
 			using (var websocket = new WebSocketHost.WebSocketHost ("56002")) {
-				Console.WriteLine ("Server started at http://localhost:56001");
+				FleckLog.Info ("Server started at http://localhost:56001");
 				// Keep window open!
-				Console.WriteLine ("Press enter to stop.!");
+				FleckLog.Info ("Press enter to stop.!");
 				Console.ReadLine ();
 			}
 
